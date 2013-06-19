89252854b024cc7e4e7497c36e94d96233900a1a
Darren Fung <darren@tunezy.com>
2013-06-19 13:18:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ifttt::Application.config.secret_token = '9811041ce36b789a678595eab7e1d34cc410d32ed5b616b07baade200a7f80391685e2c2cd5c35ce63842626735b607a2b71ca788d79183dac5ee139496c5583'
