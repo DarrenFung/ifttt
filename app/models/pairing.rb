6c83efa37928f191f18fe6193a064ea7ad245b8a
Darren Fung <darren@tunezy.com>
2013-06-29 11:58:35 -0400

    Refactors PairMaker a bit

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -14,10 +14,10 @@ class Pairing < ActiveRecord::Base
   belongs_to :user1, class_name: 'User'
   belongs_to :user2, class_name: 'User'
 
-  attr_accessible :user1_id, :user2_id
+  attr_accessible :user1, :user2
 
-  after_create :send_email
-  after_create :add_to_redis
+  after_commit :send_email
+  after_commit :add_to_redis
 
 private
 
@@ -27,10 +27,10 @@ private
 
   def add_to_redis
     # Delete the user from the user's non paired list
-    $redis.srem user_non_paired_teammates(user1.id), user2_id
+    $redis.srem user_non_paired_teammates(user1_id), user2_id
 
     # Add to paired list with the created timestamp
-    $redis.zadd user_pairings(user1.id), created_at.to_i, user2_id
+    $redis.zadd user_pairings(user1_id), created_at.to_i, user2_id
   end
 
 end
--- app/services/pair_maker.rb
+++ app/services/pair_maker.rb
@@ -8,6 +8,7 @@ class PairMaker
     @user_nonpaired_teammates = {}
     @user_paired_teammates    = {}
     @user_non_teammates       = {}
+    @all_users                = User.includes(user_teams: [:team]).select([:id, :email, :name])
   end
 
   def get_pairs
@@ -30,9 +31,11 @@ class PairMaker
 private
 
   def create_pairings
+    # Group the users so we don't have to query every user
+    users_hash = @all_users.group_by { |user| user.id }
     Pairing.transaction do
       @matching.each do |k,v|
-        Pairing.create(user1_id: k, user2_id: v)
+        Pairing.create(user1: users_hash[k].first, user2: users_hash[v].first)
       end
     end
     PairingsMailer.odd_pairing(@removed_user) unless @removed_user.nil?
@@ -46,14 +49,13 @@ private
   def users
     return @users unless @users.nil?
     @users = {}
-    all_users = User.select(:id).all.map &:id
-    User.select(:id).all.each do |u|
-      next if u.teams.count == 0
-      debugger if u.id == 117
-      @users[u.id] = @user_nonpaired_teammates[u.id].shuffle + @user_paired_teammates[u.id] + @user_non_teammates[u.id].shuffle
+    all_users = @all_users.map &:id
+    @all_users.each do |user|
+      next if user.user_teams.length == 0
+      @users[user.id] = @user_nonpaired_teammates[user.id].shuffle + @user_paired_teammates[user.id] + @user_non_teammates[user.id].shuffle
 
       # Remove non existant users
-      @users[u.id] = @users[u.id] & all_users
+      @users[user.id] = @users[user.id] & all_users
     end
     @users
   end
@@ -63,7 +65,7 @@ private
     if users.size.odd?
       # We can't have an odd pair... so find someone to remove
       user_to_remove = Random.rand(users.size)
-      user_id = users.keys[user_to_remove]
+      user_id        = users.keys[user_to_remove]
       users.delete(user_id)
       users.each do |k,v|
         v.delete(user_id)
@@ -75,42 +77,42 @@ private
   end
 
   def get_users
-    all_users = User.select(:id).all.map &:id
-    User.includes(:user_teams).select(:id).each do |u|
+    all_users = @all_users.map &:id
+    @all_users.each do |user|
 
-      debugger if u.id == 117
-      if u.teams.count == 0
-        all_users.delete u.id
+      # Delete users that don't belong to any team
+      if user.user_teams.length == 0
+        all_users.delete user.id
         next
       end
 
       # Get the user's teams
-      team_ids  = u.user_teams.map &:team_id
+      team_ids  = user.user_teams.map &:team_id
       team_keys = team_ids.map { |t| team_users(t) }
 
       # Get the teammates and assume they're non paired
       if team_keys.empty?
-        @user_nonpaired_teammates[u.id] = []
+        @user_nonpaired_teammates[user.id] = []
       else
-        @user_nonpaired_teammates[u.id] = $redis.sunion(team_keys).map &:to_i
-        @user_nonpaired_teammates[u.id].delete u.id
+        @user_nonpaired_teammates[user.id] = $redis.sunion(team_keys).map &:to_i
+        @user_nonpaired_teammates[user.id].delete user.id
       end
 
       # Get the paired teammates
-      pairing_ids                     = $redis.zrangebyscore user_pairings(u.id), '-inf', '+inf'
-      pairing_ids                     = pairing_ids.map &:to_i
+      pairing_ids                        = $redis.zrangebyscore user_pairings(user.id), '-inf', '+inf'
+      pairing_ids                        = pairing_ids.map &:to_i
 
       # This should really only include teammates, intersect the sets
-      pairing_ids                     = pairing_ids & @user_nonpaired_teammates[u.id]
+      pairing_ids                        = pairing_ids & @user_nonpaired_teammates[user.id]
 
       # Remove the paired teammates from the non-paired teammates list
-      @user_nonpaired_teammates[u.id] = @user_nonpaired_teammates[u.id] - pairing_ids
+      @user_nonpaired_teammates[user.id] = @user_nonpaired_teammates[user.id] - pairing_ids
 
-      @user_paired_teammates[u.id]    = pairing_ids
-      user_teammates                  = @user_nonpaired_teammates[u.id] + @user_paired_teammates[u.id]
+      @user_paired_teammates[user.id]    = pairing_ids
+      user_teammates                     = @user_nonpaired_teammates[user.id] + @user_paired_teammates[user.id]
 
       # Get the non-teammates
-      @user_non_teammates[u.id]       = all_users - user_teammates.map(&:to_i) - [u.id]
+      @user_non_teammates[user.id]       = all_users - user_teammates.map(&:to_i) - [user.id]
     end
   end
 
